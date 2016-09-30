b2b42e6edb2025f9d0f854ef368898c0d3f8a8e8
stbe8715 <stefan.betzmeir@elektrobit.com>
2016-09-30 17:33:58 +0200

    Added questions (not used yet). Moved dummy question generation to upper level.

--- client/main.js
+++ client/main.js
@@ -1,2 +1,28 @@
+import { Template } from 'meteor/templating';
 import './main.html';
-import './question.js';
+
+
+Template.questions.helpers({
+	questions: [
+	{
+		description: "Was ist die richtige Antwort?",
+		img: "img.jpg",
+		answers : [
+			{answer: "B", correct: false},
+			{answer: "A", correct: false},
+			{answer: "C", correct: false},
+			{answer: "Wos zum noschn!", correct: true},
+		],
+	},
+	{
+		description: "Was ist die lieblingsfarbe vom Hund?",
+		img: "img.jpg",
+		answers : [
+			{answer: "Rot", correct: false},
+			{answer: "Grün", correct: false},
+			{answer: "Blau", correct: false},
+			{answer: "Wurst", correct: true},
+		],
+	},
+	]
+});
--- client/question.js
+++ client/question.js
@@ -1,19 +1,7 @@
 import { Template } from 'meteor/templating';
-
 import './question.html';
 
-Template.question.helpers({
-	question: {
-		description: "Was ist die richtige Antwort?",
-		img: "img.jpg",
-		answers : [
-			{answer: "B", correct: false},
-			{answer: "A", correct: false},
-			{answer: "C", correct: false},
-			{answer: "Wos zum noschn!", correct: true},
-		],
-	}
-});
+
 
 Template.question.events({
 	'click .answer'(event) {
@@ -24,7 +12,7 @@ Template.question.events({
 		}
 		else
 		{
-			event.preventDefault();
+
 			alert("Quatsch!");
 		}
 	}
